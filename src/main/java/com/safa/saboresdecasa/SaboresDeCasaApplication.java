15f0ae10dd4810af7854024d7d2e54218550e2a4
David Garcia Maria <davidgama260402@gmail.com>
2024-10-22 23:25:58 +0200

    Agrego Script de creacion de base de datos

---
+++ src/main/java/com/safa/saboresdecasa/SaboresDeCasaApplication.java
@@ -0,0 +1,13 @@
+package com.safa.saboresdecasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaboresDeCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaboresDeCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safa/saboresdecasa/SaboresDeCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.saboresdecasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaboresDeCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
