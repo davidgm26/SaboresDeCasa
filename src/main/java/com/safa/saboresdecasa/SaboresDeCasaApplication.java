0019acc338f1a28fdfd3724e9d983ecdc5dce37f
David Garcia Maria <davidgama260402@gmail.com>
2024-10-22 23:26:37 +0200

    update

--- src/main/java/com/safa/saboresdecasa/SaboresDeCasaApplication.java
+++ src/main/java/com/safa/saboresdecasa/SaboresDeCasaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SaboresDeCasaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaboresDeCasaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SaboresDeCasaApplication.class, args);
+    }
 
 }
